01e32e02283a0b05cc7cd51d37f18fc15aa26a30
Mike <mike.lupo19@gmail.com>
2020-03-18 15:16:44 -0400

    added stuff

---
+++ app/src/main/java/gov/unsc/sketchio/CanvasView.java
@@ -0,0 +1,15 @@
+package gov.unsc.sketchio;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CanvasView extends View {
+
+    public CanvasView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
--- app/src/main/java/gov/unsc/sketchio/MainActivity.java
+++ app/src/main/java/gov/unsc/sketchio/MainActivity.java
@@ -2,13 +2,41 @@ package gov.unsc.sketchio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private InetAddress ipAddress;
+    private EditText nameView;
+    private String name = "Banana";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            ipAddress = InetAddress.getByAddress(new byte[]{0,0,0,0});
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        nameView = findViewById(R.id.editTextName);
     }
+
+    public void playClick(View v) {
+        if (!nameView.getText().toString().equals(""))
+            name = nameView.getText().toString();
+        Intent i = new Intent(this, PlayActivity.class);
+        i.putExtra("ipAddress", ipAddress);
+        i.putExtra("name", name);
+
+        startActivity(i);
+    }
+
 }
